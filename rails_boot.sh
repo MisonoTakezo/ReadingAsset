d70f3e2a68727de82b24cc12e87643aed6425a53
MisonoTakezo <misonotakezo1213@gmail.com>
2021-05-11 01:04:48 +0900

    docker

---
+++ rails_boot.sh
@@ -0,0 +1,8 @@
+echo "Check the rails process."
+if [ -e /usr/src/onfa/tmp/pids/server.pid ]; then
+  rm -r /usr/src/onfa/tmp/pids/server.pid
+fi
+
+echo "Start a rails server."
+bundle install -j4
+bundle exec rails s -p 3000 -b '0.0.0.0'
