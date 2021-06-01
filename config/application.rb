ad6104220790ac7cf361850f9ef36994f1390627
MisonoTakezo <misonotakezo1213@gmail.com>
2021-06-01 15:46:37 +0900

    base setting

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,15 @@ module ReadingAsset
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Tokyo'
+    # i18n
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.available_locales = :ja
+    config.i18n.default_locale = :ja
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Template engine
+    config.generators.template_engine = :slim
+    # TZ
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
