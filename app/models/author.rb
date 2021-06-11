3a29c300c208f6e2591ffc8b26c9b1f8da4b17b6
MisonoTakezo <misonotakezo1213@gmail.com>
2021-06-11 11:37:32 +0900

    author model

---
+++ app/models/author.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: authors
+#
+#  id           :bigint           not null, primary key
+#  name(著者名) :string(100)      not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+class Author < ApplicationRecord
+  validates :name, length: { maximum: 20 }, presence: true
+  
+  has_many :books_authors
+  has_many :post, through: :books_authors
+end
