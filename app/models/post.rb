107d44f0a57d81ab854d9a479dd5347fb61e3e15
MisonoTakezo <misonotakezo1213@gmail.com>
2021-06-11 11:37:54 +0900

    post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -27,6 +27,7 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 50 }, presence: true
   validates :impression, length: { maximum: 1000 }
 
+  has_one :book
   belongs_to :user
 
   enum status: {invisible: 0, visible: 1, deleted: 2 }
