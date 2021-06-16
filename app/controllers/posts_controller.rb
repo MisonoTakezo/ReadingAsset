da7385a69f6b4ba3f24ab0d12d9b583ccb826226
MisonoTakezo <misonotakezo1213@gmail.com>
2021-06-16 19:31:57 +0900

    users#edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
   before_action :must_be_post_owner, only: [:edit, :update, :destroy]
 
   def index
+    @posts = Post.all.order(id: :desc)
   end
 
   def new
