e731a7a1421f7a9346fbf55ab9972a2f72f7d1fc
MisonoTakezo <misonotakezo1213@gmail.com>
2021-06-26 12:49:42 +0900

    fix: users validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@
 
 class UsersController < ApplicationController
   before_action :logout_required, only: [:new, :create]
-  before_action :login_required, only: [:edit, :update]
-  before_action :current_user, only: [:show, :mypage]
+  before_action :login_required, only: [:mypage, :edit, :update]
+  before_action :set_user, only: [:mypage, :verify, :edit, :update]
+  before_action :should_verify_user, only: [:mypage, :edit, :update]
+  before_action :only_current_user, only: [:mypage, :edit, :update]
 
   def new
     @user = User.new
@@ -15,40 +17,30 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = flash[:success] = "まだ登録は完了してません！お送りしたメールアドレス確認用メールから登録を完了させてください。"
-      redirect_to root_path
+      render(:unverified)
     else
+      flash[:error] = "アカウントの登録に失敗しました。再度お試しください。"
       render :new
-      flash[:error] = "アカウントの登録に失敗しました。"
     end
   end
 
   def show
     @user = User.find_by(id: params[:id], status: :verified)
-
-    if @current_user && @current_user == @user
-      redirect_to mypage_user_path(@user) and return
+    unless @user
+      flash[:error] = "このアカウントは表示できません。"
+      redirect_to root_path and return
     end
-
     @posts = @user.valid_posts.page(params[:page]).per(20)
   end
 
   # GET /users/:id/mypage
   def mypage
-    @user = User.find_by(id: params[:id], status: :verified)
-
-    unless  @current_user && @current_user == @user
-      redirect_to user_path(@user) and return
-    end
-
     @posts = @user.valid_posts.page(params[:page]).per(20)
   end
 
-  def edit
-    @user = current_user
-  end
+  def edit; end
 
   def update
-    @user = current_user
     if @user.update(update_user_params)
       flash[:success] = "プロフィールを更新しました。"
       redirect_to mypage_user_path(@user)
@@ -60,8 +52,7 @@ class UsersController < ApplicationController
 
   # GET /users/:id/verify
   def verify
-    user = User.find(verify_params[:id])
-    if user.verify_by(verify_params[:vt])
+    if @user.verify_by(verify_params[:vt])
       login_and_remember(user)
       flash[:success] = "メールアドレスの確認が完了しました。プロフィールを編集してください！"
       redirect_to mypage_user_path(user)
@@ -72,6 +63,29 @@ class UsersController < ApplicationController
   end
 
   private
+
+    def set_user
+      @user = User.find(params[:id])
+
+      unless @user
+        flash[:error] = "このページは存在しません"
+        redirect_to root_path and return
+      end
+    end
+
+    def should_verify_user
+      unless @user.status_verified?
+        render(:unverified) and return
+      end
+    end
+
+    def only_current_user
+      unless current_user == @user
+        flash[:error] = "このページは表示できません"
+        redirect_to mypage_user_path(current_user) and return
+      end
+    end
+
     def create_user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
